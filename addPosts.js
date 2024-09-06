d3e89b5381a48f64b7cef1241aa4f6940654be97
Pedro Henrique <eduardofilhojj@gmail.com>
2024-09-06 10:17:05 -0300

    add files

---
+++ addPosts.js
@@ -0,0 +1,29 @@
+function returnPost(dado) {
+	const section = document.createElement('section');
+	const header = document.createElement('header');
+	const h2 = document.createElement('h2');
+	const p = document.createElement('p');
+
+	h2.textContent = dado.title;
+	p.textContent = dado.content;
+	header.appendChild(h2);
+	section.appendChild(header);
+	section.appendChild(p);
+
+	return section;
+}
+
+function insertPosts(posts, elementoPai) {
+	let conteudo = '';
+	for (let element of posts) {
+		conteudo += `
+		<section class="postInstance">
+			<header><h2>${element.title}</h2></header>
+			<p>${element.content}</p>
+		</section>`;
+	}
+	elementoPai.innerHTML = conteudo;
+}
+
+postsArea = document.getElementById("postsArea");
+insertPosts(postsData, postsArea);
---
+++ data.js
@@ -0,0 +1,14 @@
+let postsData = [{
+	title: 'Flexíveis',
+	content: 'Gatos são incrivelmente flexíveis. Seus músculos elásticos, ausência de clavícula e articulações garantem a eles uma flexibilidade excepcional. A evolução e desempenho do papel como predadores naturais deu a eles essas características, que ajudam na caça, defesa, locomoção e até redução do gasto de energia. Mesmo tranquilo na sua casa, seu gato é capaz de exibir sua elasticidade ao saltar, aterrissar após uma queda e passar por lugares apertados facilmente.'
+},{
+	title: 'Saltadores incríveis',
+	content: 'A capacidade de saltar dos gatos é, sem dúvida, uma de suas características mais impressionantes. Com suas musculosas patas traseiras e corpo extremamente flexível, eles são capazes de realizar saltos que podem chegar a várias vezes o comprimento do próprio corpo. Essa habilidade é resultado de milhões de anos de evolução, permitindo que os felinos cacem com precisão, escapem de predadores e explorem seu ambiente com agilidade. A combinação de força, flexibilidade e um sistema vestibular altamente desenvolvido torna os gatos verdadeiros mestres do salto, deixando qualquer observador maravilhado.'
+},{
+	title: 'Possuem super audição',
+	content: 'A audição dos gatos é extremamente aguçada, superando em muito a capacidade auditiva humana. Eles conseguem ouvir sons em frequências muito mais altas, permitindo-lhes detectar ruídos que são inaudíveis para nós, como os ultrassons emitidos por pequenos roedores. Essa capacidade auditiva excepcional é crucial para a caça, comunicação e sobrevivência dos felinos, permitindo-lhes localizar presas, detectar perigos e se comunicar com outros gatos a grandes distâncias. Além disso, a forma das orelhas dos gatos permite que eles direcionem a audição para diferentes fontes sonoras com grande precisão, tornando-os verdadeiros mestres da audição.'
+},{
+	title: 'Seus bigodes são radares',
+	content: 'Os bigodes dos gatos funcionam como sensores, ajudando na percepção de correntes de ar, temperatura e objetos próximos, ajudando a se orientar, caçar, no equilíbrio e até na comunicação.'
+}
+];
